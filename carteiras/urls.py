fae3fd9381f545da24b370f2a072800ad10e2035
MarcosPBackes <mpb.inv@gmail.com>
2022-07-21 22:31:58 -0300

    adalguns ajustes no app carteira

---
+++ carteiras/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+
+
+urlpatterns = [
+    path('', views.carteiras, name='carteiras'),
+    path('<int:id>', views.variavel_view, name='variavel_view'),
+    path('variavel_add/', views.variavel_add, name='variavel_add'),
+    path('variavel_edit/<int:id>/', views.variavel_edit, name='variavel_edit'),
+    path('variavel_delete/<int:id>/', views.variavel_delete, name='variavel_delete')
+]
--- carteiras/views.py
+++ carteiras/views.py
@@ -5,17 +5,58 @@ from django.contrib import messages
 
 from .forms import VariavelForm
 from .models import Variavel
+def carteiras(request):
+    return render(request, 'carteiras/carteira.html')
 
 def variavel_list(request):
-    acao_list = Variavel.objects.all().order_by('-created_at').filter(user=request.user)
+    variavel_list = Variavel.objects.all().order_by('-created_at').filter(user=request.user)
     
-    paginator = Paginator(acao_list, 10)
+    paginator = Paginator(variavel_list, 10)
     
     page = request.GET.get('page')
     
-    acao = paginator.get_page(page)
+    variavel = paginator.get_page(page)
     
-    return render(request, 'carteira/variavel.html', {'acao': acao})
+    return render(request, 'carteiras/variavel_list.html',
+                  {'variavel': variavel})
 
 def variavel_view(request, id):
+   variavel = get_object_or_404(Variavel, pk=id)
+   return render(request, 'carteiras/variavel_view.html',
+                 {'variavel': variavel})
+
+def variavel_add(request):
+    if request.method == 'POST':
+        form = VariavelForm(request.POST)
+        
+        if form.is_valid():
+            variavel = form.save(commit=False)
+            variavel.user = request.user
+            variavel.save()
+            return redirect('variavel_list')
+        else:
+            form = VariavelForm()
+            return render(request, 'carteiras/variavel_add.htlm',
+                          {'form': form})
+
+def variavel_edit(request, id):
+    variavel = get_object_or_404(Variavel, pk=id)
+    form = VariavelForm(instace=variavel)
     
+    if(request.method == 'POST'):
+        form = VariavelForm(request.POST, instance=variavel)
+        if(form.is_valid()):
+            variavel.save()
+            return redirect('variavel_list')
+        else:
+            return render(request, 'carteiras/variavel_edit.html',
+                          {'form':form, 'variavel':variavel})
+            
+def variavel_delete(request, id):
+    variavel = get_object_or_404(Variavel, pk=id)
+    variavel.delete()
+    
+    messages.info(request, 'Deletada com sucesso!')
+    
+    return redirect('variavel_list')
+
