15eafa861cf9f3aa35e528a45f4781337d5f3416
MarcosPBackes <mpb.inv@gmail.com>
2022-11-30 22:19:58 -0300

    ajustado o registro de usuarios

--- financeiro/urls.py
+++ financeiro/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sobre.urls')),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('carteiras/', include('carteiras.urls')),
 ]
