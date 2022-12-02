2f42337510e8e0d539ba5e05f31ba3bdffb9cba9
MarcosPBackes <mpb.inv@gmail.com>
2022-12-01 22:56:59 -0300

    iniciando graficos do app dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
-# Create your views here.
+def minha_carteira():
+    pass
+def renda_variavel():
+    pass
+def renda_fixa():
+    pass
+def tipos_investimento():
+    pass
+def recebimentos():
+    pass
+def pagamentos():
+    pass
--- financeiro/settings.py
+++ financeiro/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'accounts',
     'sobre',
     'carteiras',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
